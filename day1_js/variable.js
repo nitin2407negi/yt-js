c4e0fcc0599870a1d57d69f46ca13b6a7710f643
nitin2407negi <nitinnegi241@gmail.com>
2024-04-12 13:02:08 +0530

    learning variable const

---
+++ day1_js/variable.js
@@ -0,0 +1,14 @@
+const accountId = 1442  //const=no change
+let accountEmail = "nitinnegi241@gmail.com"
+var accountPassword = "12345"
+accountCity = "Indore"
+let accountState
+
+//accountId = 2 //<-not allowed
+accountEmail = "hr@gmail.com" 
+accountPassword = "212121"
+accountCity = "dehradun"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])  
