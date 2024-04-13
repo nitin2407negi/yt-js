2ec7a1e4679a5c0b295fe82d1ffc6fcce16d72ba
nitin2407negi <nitinnegi241@gmail.com>
2024-04-13 17:26:38 +0530

    conversion

---
+++ day2_datatype/conversionOpn.js
@@ -0,0 +1,23 @@
+// let score = "33"
+
+//console.log(typeof score) 
+
+// let valueInNumber = Number(score) //conversion
+//console.log(typeof valueInNumber) 
+
+// "33" => 33
+// "33abc" => NaN(not a number)
+// true => 1, false => 0
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//1 =>true 0=>false
+//""=> false
+//"nitin => true"
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
