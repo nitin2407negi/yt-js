2f45801218eb0b7173fa6cc18b0ed883ba6c120d
nitin2407negi <nitinnegi241@gmail.com>
2024-04-14 17:35:18 +0530

    all about datatypes,string,memory,math function

---
+++ day3_/math.js
@@ -0,0 +1,28 @@
+const score = 400
+// console.log(score);
+
+const balance =new Number(100.5)
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(typeof balance);
+// console.log(balance.toFixed(1));
+
+const otherNo =123.1866
+// console.log(otherNo.toPrecision(3));
+
+const hundreds=100000
+//  console.log(hundreds.toLocaleString('en-IN'));
+
+
+//maths
+// console.log(Math);
+// console.log(Math.abs(-4)); //negtive to positive only
+// console.log(Math.round(4.6)); //round figure
+
+// console.log(Math.floor(Math.random()*10) + 1);
+
+const min =10
+const max =20
+
+console.log(Math.floor(Math.random()* (max-min+1))+min)
