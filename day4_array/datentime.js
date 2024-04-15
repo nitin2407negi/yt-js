c95543ebbb80d6a8fbc1d97b589aa088a1415c11
nitin2407negi <nitinnegi241@gmail.com>
2024-04-15 11:28:41 +0530

    date and time

---
+++ day4_array/datentime.js
@@ -0,0 +1,17 @@
+let myDate=new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+let myCreateDate=new Date(2023,0,23)
+// console.log(myCreateDate.toDateString());
+
+//timestamp
+
+let myTimeStamp=Date.now()
+//console.log(myTimeStamp);
+//console.log(myCreateDate.getTime());
+//console.log(Math.floor(Date.now()/1000));
+
+let newDate=new Date()
+console.log(newDate.getDay());
