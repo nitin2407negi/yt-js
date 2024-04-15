f3a5b8f32e0cbd16281a36f34c76f4431022b151
nitin2407negi <nitinnegi241@gmail.com>
2024-04-15 12:59:40 +0530

    array 2

---
+++ day4_array/Array2.js
@@ -0,0 +1,18 @@
+const marvel_hero=['thor','ironman','spiderman']
+const hero=['hanumanji','ganeshji']
+
+
+// console.log(marvel_hero.concat(hero));
+
+const allHero=[...marvel_hero,...hero] //spread operator
+// console.log(allHero);
+
+// const nextArr=[1,2,3,[4,5,6],7,[8,9[80,90]]]
+// const realArr=nextArr.flat(Infinity)
+// console.log(realArr);
+
+let score1=100
+let score2=200
+let score=300
+console.log(Array.of(score1,score2,score));
+
