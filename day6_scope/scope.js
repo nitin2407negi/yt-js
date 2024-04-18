890621e4491d5b9ed94c450f5a2106761b62289c
nitin2407negi <nitinnegi241@gmail.com>
2024-04-18 21:20:40 +0530

    scope and hoisting

--- day6_scope/scope.js
+++ day6_scope/scope.js
@@ -4,10 +4,52 @@ if(true){
     let a=10;
     const b=20;
     var c=30;
-    console.log("inner="+a);
+    // console.log("inner="+a);
 }
 
+//{}=>scopes
+
  
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one(){
+const username="nitin";
+function two(){
+    const website="utube";
+    console.log(username);
+}
+// console.log(website);
+two();
+}
+// one();
+if(true)
+{
+    const username="negi"
+    if(username==="negi")
+    {
+        const website=" youtube "
+        // console.log(username+website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+
+
+// ++++++++++++++++++++interesting++++++++++++++++++
+
+console.log(addOne(5));
+function addOne(num){
+  return num+1
+}
+
+
+const addTwo=function(num){
+    return num+1
+}
+addTwo(2); //hoisting isko uper ni likh skte mtlb declaration se phle access nhi kr skte
+
+
+
