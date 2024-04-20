764e53c2ec36c3f98058b6d506bd48a83b6e2ea2
nitin2407negi <nitinnegi241@gmail.com>
2024-04-20 11:12:08 +0530

    while and do while loop

---
+++ day8_/whileDoWhile.js
@@ -0,0 +1,22 @@
+  let inex=0
+  while (inex <= 10) {
+    // console.log(`value of index is ${inex}`);
+    inex=inex+2
+  }
+
+  //array
+  let myARR=['flash','batman'];
+  let arr=0;
+  while(arr<myARR.length){
+    // console.log(`value is ${myARR[arr]}`);
+    arr++;
+  }
+
+  //do while loop
+ let score=11            
+  do {
+    console.log(`score is ${score}`);
+    score++;
+  } while (score<=10);
+
+  
