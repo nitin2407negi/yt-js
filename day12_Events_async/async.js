1355171eb0e8df5257f658b7e94720baecce52cc
nitin2407negi <nitinnegi241@gmail.com>
2024-04-27 11:22:02 +0530

    events and async theory

---
+++ day12_Events_async/async.js
@@ -0,0 +1,12 @@
+/*javascript=>synchronous HTMLDetailsElement,single threaded hai (default javascript)
+
+
+-->execution context=>1.execute one line of code at a time
+
+blocking code               vs        Non blocking code
+
+1.block the flow of program           1.does not block execution
+2.Read file Sync                      2.Read file async
+
+
+*/
